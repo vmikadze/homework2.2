27b5f3cd27b13d8b8509c2c99d1aa217928a9c1d
Vladimir Mikadze <v.mikadze10@gmail.com>
2023-02-12 23:22:25 +0600

    task done

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int startBalance = 100;
+        int paid = 1200;
+        int finalBalance;
+        int bonus;
+
+        if (paid > 1000) {
+            bonus = paid / 100;
+            finalBalance = startBalance + bonus;
+        }
+        else {
+            finalBalance = startBalance;
+            bonus = 0;
+        }
+
+        System.out.println("Баланс = " + finalBalance + " рублей");
+        System.out.println("Размер бонуса = " + bonus + " рублей");
+    }
+}
